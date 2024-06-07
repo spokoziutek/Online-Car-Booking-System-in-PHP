205ffe06816ba2759197d835937ed5c7abd85141
Łukasz Bieskiewicz <122742419+spokoziutek@users.noreply.github.com>
2024-06-07 12:10:45 +0200

    Update config.php

--- admin/vendor/inc/config.php
+++ admin/vendor/inc/config.php
@@ -4,10 +4,10 @@ Github Link: https://github.com/dev-mhrony
 Youtube Link: https://www.youtube.com/channel/UChYhUxkwDNialcxj-OFRcDw
 -->
  <?php
-$dbuser="root";
-$dbpass="";
-$host="localhost";
-$db="OnlineCarBooking";
+$dbuser="mphwdngpxl";
+$dbpass="qweasdzxc1!";
+$host="rantacar-server.mysql.database.azure.com";
+$db="rantacar-database";
 $mysqli=new mysqli($host,$dbuser, $dbpass, $db);
 ?>
  <!-- Author By: MH RONY
