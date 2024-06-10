0dabc03ffa7b8bbc90c20716aa3af0d258fc7a07
Łukasz Bieskiewicz <122742419+spokoziutek@users.noreply.github.com>
2024-06-10 19:10:41 +0200

    Update user-logout.php
    testing

--- usr/user-logout.php
+++ usr/user-logout.php
@@ -3,6 +3,12 @@ Author Website: https://developerrony.com
 Github Link: https://github.com/dev-mhrony
 Youtube Link: https://www.youtube.com/channel/UChYhUxkwDNialcxj-OFRcDw
 --><?php
+
+    // Włącz wyświetlanie błędów (dla celów debugowania)
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
     session_start();
     unset($_SESSION['u_id']);
     session_destroy();
